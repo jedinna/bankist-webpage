f28426afbbe660dc8ffe7e78159c7b42ee4b68f5
yididya kebede <yididyakebede884@gmail.com>
2024-06-05 23:37:18 +0300

    add stickey nav

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScroollTo = document.querySelector('.btn--scroll-to');
 const sectiion1 = document.querySelector('#section--1');
 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+const nav = document.querySelector('.nav');
+
 ///////////////////////////////////////
 // Modal window
 const openModal = function (e) {
@@ -70,9 +76,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 });
 
 //tabbed components
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
@@ -91,4 +94,30 @@ tabsContainer.addEventListener('click', function (e) {
 });
 
 //menu fade animation
-const nav = document.querySelector('.nav');
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+
+//stickey navigation
+
+const initialcoords = sectiion1.getBoundingClientRect();
+console.log(initialcoords);
+
+window.addEventListener('scroll',function(){
+  if (this.window.scrollY > initialcoords.top)nav.classList.add('sticky')
+    else nav.classList.remove('sticky')
+})
