fdde18c9e574b7267ce8a1d91d6a6ac11a8ce36f
yididya kebede <yididyakebede884@gmail.com>
2024-06-05 22:18:29 +0300

    build tabbed component

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScroollTo = document.querySelector('.btn--scroll-to');
+const sectiion1 = document.querySelector('#section--1');
 
+///////////////////////////////////////
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -18,7 +19,7 @@ const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
-btnsOpenModal.forEach(btn=> btn.addEventListener('click', openModal));
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -46,13 +47,9 @@ document.addEventListener('keydown', function (e) {
 
 // message.style.height = Number.parseFloat(getComputedStyle(message).height,10) + 40 +'px'
 
-
-const btnScroollTo = document.querySelector('.btn--scroll-to');
-const sectiion1 = document.querySelector('#section--1');
-
-btnScroollTo.addEventListener('click',function(e){
-sectiion1.scrollIntoView({behavior:'smooth'});
-})
+btnScroollTo.addEventListener('click', function (e) {
+  sectiion1.scrollIntoView({ behavior: 'smooth' });
+});
 
 // document.querySelectorAll('.nav__link').forEach(function(el){
 //   el.addEventListener('click',function(e){
@@ -64,12 +61,34 @@ sectiion1.scrollIntoView({behavior:'smooth'});
 //   })
 // })
 
-document.querySelector('.nav__links').addEventListener('click',
-  function(e){
-    e.preventDefault();
-    if(e.target.classList.contains('nav__link')){
-      const id = e.target.getAttribute('href');
-      document.querySelector(id).scrollIntoView({behavior:'smooth'});
-    }
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
-)
+});
+
+//tabbed components
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  if (!clicked) return;
+  //removing active class
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'));
+  //activate tab
+  clicked.classList.add('operations__tab--active');
+  //activating content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+  // .classList.add('operations__content--active');
+});
+
+//menu fade animation
+const nav = document.querySelector('.nav');
