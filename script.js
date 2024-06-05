c524cfd45088d8455722d17faee793e72ebffc8b
yididya kebede <yididyakebede884@gmail.com>
2024-06-05 17:51:57 +0300

    scrolling navigations

--- script.js
+++ script.js
@@ -53,3 +53,23 @@ const sectiion1 = document.querySelector('#section--1');
 btnScroollTo.addEventListener('click',function(e){
 sectiion1.scrollIntoView({behavior:'smooth'});
 })
+
+// document.querySelectorAll('.nav__link').forEach(function(el){
+//   el.addEventListener('click',function(e){
+//     e.preventDefault();
+//     console.log('link');
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({behavior:'smooth'});
+//   })
+// })
+
+document.querySelector('.nav__links').addEventListener('click',
+  function(e){
+    e.preventDefault();
+    if(e.target.classList.contains('nav__link')){
+      const id = e.target.getAttribute('href');
+      document.querySelector(id).scrollIntoView({behavior:'smooth'});
+    }
+  }
+)
